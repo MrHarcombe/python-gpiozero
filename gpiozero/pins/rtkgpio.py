8e404af9cf7eed83dc6fe01989599a1b2a43460e
Ryan Walmsley <ryanteck@gmail.com>
2017-03-13 11:25:12 +0000

    Cake

--- gpiozero/pins/rtkgpio.py
+++ gpiozero/pins/rtkgpio.py
@@ -81,7 +81,7 @@ class RTkGPIOPin(LocalPin):
             GPIO.setmode(GPIO.BCM)
             GPIO.setwarnings(False)
         if cls.PI_INFO is None:
-            cls.PI_INFO = pi_info(900092)
+            cls.PI_INFO = pi_info("900092")
             #Fake a Pi Zero 1.2, Closest device as just has GPIO Pins and no camera.
             #cls.PI_INFO = 900092
         try:
